fbde619b11b0ff1887ec8077f60291ada860cc91
olivier thollas <o_thollas@hotmail.com>
2023-08-22 13:20:30 +0200

    ajout home et catchphrase

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Button from './Button';
 
-const Card = ({ title, content, image, className }) => {
+const Card = ({ title, image, className }) => {
   return (
     <div className={className}>
       <img src={image} alt="Card image" />
       <h2>{title}</h2>
-      <p>{content}</p>
-      <Button/>
     </div>
   );
 };
